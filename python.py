f1434860888e9b221514450d176097995505d0da
Nevindu622 <nevindu@smartannualreport.com>
2026-06-19 10:01:02 +0530

    add

--- python.py
+++ python.py
@@ -3,10 +3,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# API key එක දාන්න
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-# ඔයාට පාවිච්චි කරන්න පුළුවන් මොඩල් ලැයිස්තුව බලන්න
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
         print(m.name)
